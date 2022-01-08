8f97168f04135fa147a8ef525beb654070b3665c
lucasdalan <lucasdalan09@gmail.com>
2022-01-08 19:26:22 -0300

    aaa

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://lucasdalan:lucas1234@even-odd.kz5ox.mongodb.net/test',
+    ),
+    UsersModule,
+  ],
   controllers: [],
   providers: [],
 })
--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,5 +1,5 @@
 export class CreateUserDto {
-  nome: string;
-  senha: string;
+  name: string;
+  password: string;
   address: string;
 }
