9a2565f7a27d63eab3ab864fa69e3c6dab259e51
lucasdalan <lucasdalan09@gmail.com>
2022-01-11 21:58:51 -0300

    Rotas finalizadas (Deploy / Test)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,12 @@
-import { Module } from '@nestjs/common';
-import { UsersModule } from './users/users.module';
-import { MongooseModule } from '@nestjs/mongoose';
-import { AuthModule } from './auth/auth.module';
+import { Module } from "@nestjs/common";
+import { UsersModule } from "./users/users.module";
+import { MongooseModule } from "@nestjs/mongoose";
+import { AuthModule } from "./auth/auth.module";
 
 @Module({
   imports: [
     MongooseModule.forRoot(
-      'mongodb+srv://lucasdalan:lucas1234@even-odd.kz5ox.mongodb.net/test',
+      "mongodb+srv://lucasdalan:lucas1234@even-odd.kz5ox.mongodb.net/test"
     ),
     UsersModule,
     AuthModule,
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -8,17 +8,17 @@ import {
   Delete,
   UseGuards,
   Request,
-} from '@nestjs/common';
-import { UsersService } from './users.service';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
-import { AuthGuard } from '@nestjs/passport';
+} from "@nestjs/common";
+import { UsersService } from "./users.service";
+import { CreateUserDto } from "./dto/create-user.dto";
+import { UpdateUserDto } from "./dto/update-user.dto";
+import { AuthGuard } from "@nestjs/passport";
 
-@Controller('users')
+@Controller("users")
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
-  @Post()
+  @Post("register")
   create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
   }
@@ -28,23 +28,39 @@ export class UsersController {
     return this.usersService.findAll();
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
+  @Get("deploy")
+  async deployContract() {
+    const shell = await import("shelljs");
+    shell.cd("C:/Users/PC/Documents/Projetos/nestjs-auth");
+    const deploy = shell.exec("npx hardhat run scripts/deploy.js");
+    return deploy;
+  }
+
+  @Get("test")
+  async testContract() {
+    const shell = await import("shelljs");
+    shell.cd("C:/Users/PC/Documents/Projetos/nestjs-auth");
+    const deploy = shell.exec("npx hardhat test");
+    return deploy;
+  }
+
+  @Get(":id")
+  findOne(@Param("id") id: string) {
     return this.usersService.findOne(id);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+  @Patch(":id")
+  update(@Param("id") id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.usersService.update(id, updateUserDto);
   }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
+  @Delete(":id")
+  remove(@Param("id") id: string) {
     return this.usersService.remove(id);
   }
 
-  @UseGuards(AuthGuard('local'))
-  @Post('login')
+  @UseGuards(AuthGuard("local"))
+  @Post("login")
   async login(@Request() req) {
     return req.user;
   }
